549359a3ee89ffc5a5d97c9fcbc27ff6f62c163f
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-11-20 23:24:38 -0300

    Métodos de requisição HTTP

---
+++ api/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+// acima estamos instanciando o express
+// e armazenando na constante express
+
+const app = express()
+// acima estamos executando o express
+// e armazenando na constante app
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
+// escutando na porta 3000
+// para iniciar  o servidor, é só digitar node index.js ou node .
+
+app.route('/').get((req, res) => {
+    res.send('Hello World')
+})
+// rota para localhost:3000/ ou rota raiz
+// .get é método buscar / pegar
+// req de requisão e res de resposta
+// res.send == enviar uma resposta
+// para o server e inicie novamente
+
+app.route('/sobre').get((req, res) => {
+    res.send('Sobre Mim')
+    res.send(req.query.name)
+})
+
+// middleware JSON
+app.use(express.json())
+// abra o insomia para executar os métodos
+
+// método post == enviar informação
+app.route('/').post((req, res) => {
+    res.send(req.body)
+    console.log(req.body)
+})
+
+// método put == editar informação
+let author = 'marcelo'
+
+app.route('/autor').get((req, res) => res.send(author))
+
+app.route('/autor').put((req, res) => {
+    author = req.body.author
+    res.send(author)
+    console.log(req.body)
+})
+
+// método delete == deletar informação
+app.route('/autor/:id').delete((req, res) => {
+    id = req.params.id
+    res.send(id)
+    console.log(req.params.id)
+})
