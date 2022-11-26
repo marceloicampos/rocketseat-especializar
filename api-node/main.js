6d4f92d628f0ae562f0e9f1eb5062efe14e4a79c
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-11-25 22:01:23 -0300

    use fetch on method GET

--- api-node/main.js
+++ api-node/main.js
@@ -4,6 +4,7 @@ function getUsers() {
     fetch(url)
         .then(response => response.json())
         .then(data => (renderApiResult.textContent = JSON.stringify(data)))
+        // o JSON.stringify é um método converte um valor JavaScript em uma string JSON
         .catch(error => console.error(error))
 }
 
