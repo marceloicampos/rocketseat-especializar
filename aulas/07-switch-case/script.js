0dd9c7d96420444a9d4b2b1eda4174231a2e7145
marceloicampos <marcelo.ildefonso@hotmail.com>
2022-12-27 13:48:09 -0300

    switch case

---
+++ aulas/07-switch-case/script.js
@@ -0,0 +1,18 @@
+let nota = 11
+
+switch (nota) {
+    case 10:
+        console.log('Dez')
+        break
+    case 9:
+        console.log('Nove')
+        break
+    case 8:
+    case 7:
+    case 6:
+        console.log('Oito, Sete ou Seis')
+        break
+    default:
+        console.log('Nota inválida')
+        break
+}
