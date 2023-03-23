a0b429204248f80c8f0d81e3d5765d457a8d9477
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-03-23 01:40:12 -0300

    update index.js

--- api-serie-node/src/index.js
+++ api-serie-node/src/index.js
@@ -8,14 +8,13 @@ const userModel = require('./app/models/user')
 app.use(express.json())
 // usando o parse do express para o json
 
-require('./app/controllers/index.js')(app)
+require('./app/controllers/index')(app)
 // importamos apenas o controller index e este importa todos os controllers
 // require('./app/controllers/authController')(app)
 // require('./app/controllers/projectController')(app)
 // estamos requerendo o authController e
 // estamos referenciando o controle de autenticação com o (app) e estamos repassando para os controllers ao nosso app
 
-app.listen(3000, () => console.log('Node Express Server on-line on port 3000'))
 app.get('/', (req, res) =>
     res.send(
         '<head><style>body{background-color:black}p{color: #9400D3; font-size: 1.5em;padding:32px}</style></head><p>Node Express Server on-line on port 3000, with Mongo DB Atlas. Users on DB: <a href="http://localhost:3000/users">localhost:3000/users</a></p>'
@@ -29,6 +28,7 @@ app.get('/users', async (request, response) => {
         response.status(500).send({ error: 'Find User on Server failed' })
     }
 })
+app.listen(3000, () => console.log('Node Express Server on-line on port 3000'))
 // listen 3000 é um escutador de rotas em localhost:3000 que exibe um console no servidor
 // a rota get '/' é a rota principal que envia uma resposta simples para o navegador cliente
 // a rota get '/users' é a rota de listagem de usuários cadastrados no mongoDB Atlas
