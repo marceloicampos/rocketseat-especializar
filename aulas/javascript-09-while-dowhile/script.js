eedd8590059712880a543253de55a9df13df0165
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-07-19 08:23:41 -0300

    javascript while dowhile

---
+++ aulas/javascript-09-while-dowhile/script.js
@@ -0,0 +1,45 @@
+let n = 0
+
+while (n < 10) {
+    console.log(n)
+    n++
+    //n=n+1
+    //n+=1
+}
+
+console.log('---')
+
+let fatorM = 5
+let multiplicador = 1
+
+while (multiplicador < 6) {
+    multiplex = fatorM * multiplicador++
+    console.log(multiplex)
+}
+
+console.log('---')
+
+let fatorF = 1
+let fatorial = 5
+
+while (fatorial >= 1) {
+    fatorF = fatorF * fatorial
+    fatorial--
+}
+console.log(fatorF)
+
+console.log('---')
+
+n = 0
+m = 1
+
+do {
+    r = n + m
+    n++
+    console.log(r)
+} while (r <= 9)
+
+// no DO WHILE temos a garantia de pelo menos uma execução de loop
+// faça e depois verifique
+// no WHILE não há garantia de execução de loop
+// verifique e depois faça
