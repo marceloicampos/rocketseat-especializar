8647579ceaf42c7c096bc8f24bfb3a42772abfd1
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-07-21 15:45:31 -0300

    javascript generator function

---
+++ aulas/javascript-12-generator-function/script.js
@@ -0,0 +1,38 @@
+function* cores() {
+    yield 'vermelho'
+    yield 'verde'
+    yield 'azul'
+}
+
+const iteratorCor = cores()
+
+console.log(iteratorCor.next().value)
+console.log(iteratorCor.next().value)
+console.log(iteratorCor.next().value)
+console.log(iteratorCor.next().value)
+
+function* perguntas() {
+    const nome = yield 'Qual seu nome ? '
+    const esporte = yield 'Qual seu esporte ? '
+    return 'Seu nome é ' + nome + ' e seu esporte é ' + esporte
+}
+
+const iteratorPerguntas = perguntas()
+
+console.log(iteratorPerguntas.next().value)
+console.log(iteratorPerguntas.next('Marcelo').value)
+console.log(iteratorPerguntas.next('Corrida').value)
+
+function* contador() {
+    let i = 0
+    while (true) {
+        yield i++
+    }
+}
+
+const iteratorContador = contador()
+
+console.log(iteratorContador.next().value)
+console.log(iteratorContador.next().value)
+console.log(iteratorContador.next().value)
+console.log(iteratorContador.next().value)
