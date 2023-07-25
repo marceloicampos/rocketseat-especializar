39c4c35632214b774ad010b36f8a5556bc7fee12
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-07-25 01:47:35 -0300

    javascript map method

--- aulas/javascript-13-map-method/script.js
+++ aulas/javascript-13-map-method/script.js
@@ -1,20 +1,18 @@
-const cursos = [
-    'HTML', 'CSS', 'Java', 'PHP', 'React'
-]
+const cursos = ['HTML', 'CSS', 'Java', 'PHP', 'React']
 
-cursos.map((element, index, array)=> {
-    console.log('Curso: ' + element + ' | Posição do Curso: ' + index + ' | Lista de Todos os Cursos: ' + array);
+cursos.map((element, index, array) => {
+    console.log('Curso: ' + element + ' | Posição do Curso: ' + index + ' | Lista de Todos os Cursos: ' + array)
 })
 
-console.log('---')
+console.log('1-------------')
 
 let c = cursos.map((el, i) => {
-    return '<div>' + el + '</div>'
+    return '<div>' + el + ' ' + i + '</div>'
 })
 
 console.log(c)
 
-console.log('---')
+console.log('2-------------')
 
 let el = document.getElementsByTagName('div')
 
@@ -22,22 +20,28 @@ el = [...el]
 
 console.log(el)
 
-el.map((el)=> {
-    el.innerHTML = 'Cursos'
-    console.log(el.innerHTML)
+el.map((el, i) => {
+    console.log(el.innerHTML + ' ' + i)
 })
 
-console.log('---')
+console.log('---');
+
+el.map((el, i) => {
+    el.innerHTML = 'Cursos ' + i
+    console.log(el.innerHTML);
+})
+
+console.log('3-------------')
 
 const elementos = document.getElementsByTagName('div')
-const val = Array.prototype.map.call(elementos, ({innerHTML})=> innerHTML)
-console.log(val);
+const val = Array.prototype.map.call(elementos, ({ innerHTML }) => innerHTML)
+console.log(val)
 
-console.log('---')
+console.log('4-------------')
 
-const converterInt = (e) => parseInt(e)
-const dobro = (e) => e*2
+const converterInt = e => parseInt(e)
+const dobro = e => e * 2
 let num = ['1', '2', '3', '4', '5'].map(converterInt)
 let dob = ['1', '2', '3', '4', '5'].map(dobro)
-console.log(num);
-console.log(dob);
+console.log(num)
+console.log(dob)
