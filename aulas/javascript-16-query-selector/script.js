aa82215faa4e810e88037cc16712bf94ef0cc44f
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-07-28 08:48:44 -0300

    javascript query selector

--- aulas/javascript-16-query-selector/script.js
+++ aulas/javascript-16-query-selector/script.js
@@ -1,2 +1,14 @@
-const nome = 'Marcelo'
-console.log(nome)
+const queryDiv = document.querySelector('.curso')
+console.log(queryDiv)
+
+const queryAllDivTodas = document.querySelectorAll('.curso, p')
+console.log(queryAllDivTodas)
+
+const queryAllDivArray = [...document.querySelectorAll('.curso, p')]
+console.log(queryAllDivArray)
+
+const queryAllDivClass = [...document.querySelectorAll('div[class]')]
+console.log(queryAllDivClass)
+
+const queryAllDivP = [...document.querySelectorAll('div>p')][0]
+console.log(queryAllDivP)
