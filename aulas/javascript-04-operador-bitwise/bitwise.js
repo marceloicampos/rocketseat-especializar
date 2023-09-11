b996f1ee1b32973e476e95e9e858212b98d32bc8
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-09-11 11:01:13 -0300

    Update bitwise.js

--- aulas/javascript-04-operador-bitwise/bitwise.js
+++ aulas/javascript-04-operador-bitwise/bitwise.js
@@ -1,20 +1,25 @@
-// 10 == 1010
-// 11 == 1011
-
+// 10 == 00001010
+// 11 == 00001011
 let n1 = 10
 let n2 = 11
+let n32 = 32
 let resAnd = n1 & n2 // 1010 = 10
-let resOr = n1 | n2 // 1011 = 10
+// tabela verdade do E
+let resOr = n1 | n2 // 1011 = 11
+// tabela verdade do OU
 let resXor = n1 ^ n2 // 0001 = 1
+// tabela verdade do OU EXCLUSIVO
 // acima: onde tiver equivalência de bits retorno 0 e onde for diferente retorno 1
 // ou exclusivo somente é V se for um ou outro, ou seja, se for v f ou f v
-let resBl = n1 << 1 // 10100
+let resBl = n1 << 1 // 00010100 == 10100
 // deslocando 1 bit para a esquerda (dobrar um valor)
-let resBr = n1 >> 1 // 101
+let resBr = n1 >> 1 // 00000101 == 101
 // deslocando 1 bit para a direita (dividir uma valor pela metade)
+let res64 = n32 << 1
 
 console.log(resAnd)
 console.log(resOr)
 console.log(resXor)
 console.log(resBl)
 console.log(resBr)
+console.log(res64)
