304fd63e1575c944fb72ee71947766cf1e5516e4
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-09-12 09:11:48 -0300

    aula02-ts

---
+++ aulas/typescript-01-intro/aula02.js
@@ -0,0 +1,7 @@
+var nome = 'Marcelo';
+var canal = 'CursoTS';
+var curso = 'Typescript';
+console.log(nome);
+console.log(canal);
+console.log(curso);
+// npx tsc aula02.ts
---
+++ aulas/typescript-01-intro/aula02.ts
@@ -0,0 +1,9 @@
+var nome = 'Marcelo'
+var canal = 'CursoTS'
+var curso = 'Typescript'
+
+console.log(nome)
+console.log(canal)
+console.log(curso)
+
+// npx tsc aula02.ts
