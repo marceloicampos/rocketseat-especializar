68abbdd11e1dabacd2011ac99dc0cd7b7ad4cc34
marceloicampos <marcelo.ildefonso@hotmail.com>
2024-01-03 11:16:28 -0300

    scope of variables and constants

--- aulas/react-next-project-01/project-01/src/pages/index.tsx
+++ aulas/react-next-project-01/project-01/src/pages/index.tsx
@@ -3,12 +3,26 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const name = 'marcelo'
+let canal = 'curso'
+
+function retNameOut() {
+  return name
+}
+
 export default function Home() {
+  function retNameIn() {
+    return name
+  }
   return (
     <main
       className={`${inter.className}`}
     >
       <p>Hello World !!!</p>
+      <p>{name}</p>
+      <p>{canal}</p>
+      <p>{retNameOut()}</p>
+      <p>{retNameIn()}</p>
     </main>
   )
 }
